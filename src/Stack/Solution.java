7c2f69e46bde52818c69f782dfd7f125b2bf1b6a
Ryan <ryan@example.com>
2019-04-23 10:06:04 +0800

    Initial commit

---
+++ src/Stack/Solution.java
@@ -0,0 +1,41 @@
+package Stack;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+
+                if (stack.empty())
+                    return false;
+
+                if (c == ')' && stack.pop() != '(') {
+                    return false;
+                }
+
+                if (c == '}' && stack.pop() != '{') {
+                    return false;
+                }
+
+                if (c == ']' && stack.pop() != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).isValid("()[]"));
+        System.out.println((new Solution()).isValid(""));
+        System.out.println((new Solution()).isValid("{}[]"));
+    }
+}
