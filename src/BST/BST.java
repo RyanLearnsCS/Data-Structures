11a3cfb01060c0465b332271e1d038dc4e879996
Ryan <ryan@example.com>
2019-04-29 16:13:57 +0800

    improve add

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -31,19 +31,13 @@ public class BST<E extends Comparable<E>> {
     }
 
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size ++;
-        }
-
-        else
-            add(root, e);
-
+        root = add(root, e);
     }
 
     // 向以node为根的二分搜索树中插入元素E，递归算法
-    private void add(Node node, E e) {
-        if (e.equals(node.e))
+    //返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e) {
+/*        if (e.equals(node.e))
             return;
         else if (e.compareTo(node.e) < 0 && node.left == null) {
             node.left = new Node(e);
@@ -59,7 +53,20 @@ public class BST<E extends Comparable<E>> {
         if (e.compareTo(node.e) < 0)
             add(node.left, e);
         else
-            add(node.right, e);
+            add(node.right, e);*/
+        if (node == null) {
+            size ++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if (e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
     }
+
+
 
 }
