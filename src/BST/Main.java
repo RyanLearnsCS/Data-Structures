f6a551492f12a63f4156569b1da04392512a0f54
Ryan <ryan@example.com>
2019-05-06 15:15:08 +0800

    Binary Search Tree

--- src/BST/Main.java
+++ src/BST/Main.java
@@ -1,22 +1,48 @@
 package BST;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         BST<Integer> bst = new BST<Integer>();
-        int[] nums = {5, 3, 6, 8, 4, 2};
+        Random random = new Random();
+
+        int n = 1000;
+        for (int i = 0; i < n; i ++) {
+            bst.add(random.nextInt(10000));
+        }
+            ArrayList<Integer> nums = new ArrayList<>();
+            while (!bst.isEmpty())
+                nums.add(bst.removeMin());
+
+            System.out.println(nums);
+
+            for (int i = 1; i < nums.size() ; i ++) {
+                if (nums.get(i-1) > nums.get(i))
+                    throw new IllegalArgumentException("Error");
+            }
+            System.out.println("removeMin test completed.");
+
+
+
+/*        int[] nums = {5, 3, 6, 8, 4, 2};
         for (int num: nums)
             bst.add(num);
 
-        bst.preOrder();
+        bst.levelOrder();
+
+*//*        bst.preOrder();
 
         System.out.println();
         //System.out.println(bst);
+        bst.preOrderNR();*//*
 
-        bst.inOrder();
+*//*        bst.inOrder();
         System.out.println();
 
         bst.postOrder();
-        System.out.println();
+        System.out.println();*/
     }
 }
